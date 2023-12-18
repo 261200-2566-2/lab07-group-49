4fb1cb0f521c8505f633592ae3291043ee33f197
NN <rawipa_s@cmu.ac.th>
2023-12-18 16:24:29 +0700

    submit1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,70 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        Lab07.test();
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    static class Lab07{
+        static void test(){
+            Set<String> s1 = new Set<>();
+            Set<String> s2 = new Set<>();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            s1.add("OOP");
+            s1.add("ez");
+            s1.add("Lab07");
+            s1.add("hello world!");
+            s1.add("ez");
+            for(String a : s1){
+                System.out.print(a + " "); //print all members in s1
+            }
+            System.out.println();
+
+            s1.remove("ez");
+            System.out.println(s1.contains("ez")); //false
+
+            s2.add("7");
+            s2.add("1");
+            s2.add("3");
+            s2.add("100");
+            s2.remove("50");
+            for(String a : s2){
+                System.out.print(a + " "); //print all members in s2
+            }
+            System.out.println();
+
+            System.out.println(s2.contains("3")); //true
+
+            s2.addAll(s1);
+            for(String a : s2){
+                System.out.print(a + " "); //print all members in s2 (after add all members in s1)
+            }
+            System.out.println();
+
+            s1.remove("OOP");
+            for(String a : s1){
+                System.out.print(a + " "); //print all members in s1 (after remove "ez" and "OOP")
+            }
+            System.out.println();
+
+            s1.addAll(s2);
+            for(String a : s1){
+                System.out.print(a + " "); //print all members in s1 (after add all members in s2)
+            }
+            System.out.println();
+
+            s1.removeAll(s2);
+            System.out.println(s1.containsAll(s2)); //false
+            System.out.println(s2.containsAll(s1)); //true
+
+            for(String a : s1){
+                System.out.print(a + " "); //print all members in s1 (none)
+            }
+            System.out.println();
+
+            s2.removeAll(s1);
+            for(String a : s2){
+                System.out.print(a + " "); //print all members in s2 (after remove all members in s1)
+            }
+            System.out.println();
         }
     }
 }
